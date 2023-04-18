8cd2719e519cad910d44ce7907ee95d44dc625de
Jorge <portalmty@outlook.es>
2023-04-18 14:32:17 -0600

    Login

---
+++ SYSTEM_INVENTARIO_MAGENTA_DIGITAL/ACCESO/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL.ACCESO
+{
+    class Login
+    {
+        private dynamic usuario = "usAdmin";
+        private dynamic contraseña = "Passw0rd";
+
+        public bool validarLogin(dynamic user, dynamic pass)
+        {
+            bool login = false;
+            if (user == usuario)
+            {
+                if (pass == contraseña)
+                {
+                    login = true;
+                }
+                else
+                {
+                    MessageBox.Show("La contaseña ingresada es incorrecta");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El usuario ingresado es incorrecto");
+            }
+
+            return login;
+        }
+    }
+}
