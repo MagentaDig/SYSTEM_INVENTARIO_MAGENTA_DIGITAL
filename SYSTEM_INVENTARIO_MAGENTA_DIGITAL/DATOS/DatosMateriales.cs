c8d5b9a56bd5d61d286979251a9568596e147be5
Jorge <jorgepm914@gmail.com>
2023-04-25 03:31:19 -0600

    CAMBIOS 03:31

---
+++ SYSTEM_INVENTARIO_MAGENTA_DIGITAL/DATOS/DatosMateriales.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using SYSTEM_INVENTARIO_MAGENTA_DIGITAL.DB_CONEXION;
+using SYSTEM_INVENTARIO_MAGENTA_DIGITAL.MODELOS;
+
+namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL.DATOS
+{
+    internal class DatosMateriales
+    {
+        Conexion conn = new Conexion();
+        SqlCommand cmd = new SqlCommand();
+
+        public void AgregarMaterial (MMateriales Material)
+        {
+            cmd.Connection = conn.AbrirConexion();
+            cmd.CommandText = "SP_AGREGAR_MATERIAL";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Descripcion", Material.Descripcion);
+            cmd.Parameters.AddWithValue("@Id_Categ", Material.Categoria);
+            cmd.Parameters.AddWithValue("@Nombre", Material.Nombre);
+            cmd.Parameters.AddWithValue("@Tamaño", Material.Tamaño);
+            cmd.Parameters.AddWithValue("@Metros", Material.Metros);
+            cmd.Parameters.AddWithValue("@Cantidad", Material.Cantidad);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader MostrarMateriales (int Categoria)
+        {
+            cmd.Connection = conn.AbrirConexion();
+            cmd.CommandText = "SP_MOSTRAR_MATERIALES";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@idCategoria", Categoria);
+
+            SqlDataReader datosMateriales = cmd.ExecuteReader();
+
+            return datosMateriales;
+        }
+    }
+}
---
+++ SYSTEM_INVENTARIO_MAGENTA_DIGITAL/MODELOS/MMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL.MODELOS
+{
+    class MMateriales
+    {
+        public int IdMaterial { get; set; }
+        public dynamic Nombre { get; set; }
+        public dynamic Tamaño { get; set; }
+        public decimal Metros { get; set; }
+        public int Cantidad { get; set; }
+        public dynamic Descripcion { get; set; }
+        public dynamic Categoria { get; set; }
+    }
+}
