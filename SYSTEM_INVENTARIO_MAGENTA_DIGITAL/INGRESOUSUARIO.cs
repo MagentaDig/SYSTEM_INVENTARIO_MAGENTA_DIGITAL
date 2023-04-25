98930423ab95ab9a1d8eea9eaddcec95d4f56426
KEILA ELIZABETH VILLANUEVA PEREZ <KVILLANUEVAPE@uanl.edu.mx>
2023-04-25 00:23:29 -0600

    CAMBIOS 25 ABR

--- SYSTEM_INVENTARIO_MAGENTA_DIGITAL/INGRESOUSUARIO.cs
+++ SYSTEM_INVENTARIO_MAGENTA_DIGITAL/INGRESOUSUARIO.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SYSTEM_INVENTARIO_MAGENTA_DIGITAL.ACCESO;
 
 namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL
 {
@@ -23,6 +24,33 @@ namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL
         }
 
         private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            Login log = new Login();
+
+            dynamic usuario = txt_usuario.Text;
+            dynamic pass = txt_contraseña.Text;
+
+            bool acceso = log.validarLogin(usuario, pass);
+
+            if (acceso)
+            {
+                this.Hide();
+                INGRESO formIngreso = new INGRESO();
+                formIngreso.Show();
+            }
+        }
+
+        private void txt_usuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_contraseña_TextChanged(object sender, EventArgs e)
         {
 
         }
--- SYSTEM_INVENTARIO_MAGENTA_DIGITAL/INICIO.cs
+++ SYSTEM_INVENTARIO_MAGENTA_DIGITAL/INICIO.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL
 {
@@ -18,6 +19,56 @@ namespace SYSTEM_INVENTARIO_MAGENTA_DIGITAL
         {
             InitializeComponent();
             this.idCateg = id;
+        }
+
+        [DllImport("User32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("User32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void btnslide_Click(object sender, EventArgs e)
+        {
+            if (panelmenuvertical.Width == 250)
+            {
+                panelmenuvertical.Width = 70;
+            }
+            else
+                panelmenuvertical.Width = 250;
+        }
+
+        private void iconocerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void iconomaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            iconorestaurar.Visible = true;
+            iconomaximizar.Visible = false;
+        }
+
+        private void iconorestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            iconorestaurar.Visible = false;
+            iconominimizar.Visible = true;
+        }
+
+        private void iconominimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel_barratitulo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
